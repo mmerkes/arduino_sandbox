8f38f536e53e85eb3afbb78235ac66264e4db1ad
Empty Arsenal <matt.merkes@gmail.com>
2013-06-06 14:21:03 -0700

    deleted dupe

--- LED_5x5v2/LED_5x5v2.ino
+++ LED_5x5v2/LED_5x5v2.ino
@@ -1,13 +1,40 @@
 int rows[5] = {8, 9, 10, 11, 12}; //pin numbers
 int cols[5] = {2, 3, 4, 5, 6}; //pin numbers
 
-int smiley[5][5] = {
-  {LOW, HIGH, LOW, HIGH, LOW},
-  {LOW, LOW, LOW, LOW, LOW},
-  {LOW, LOW, HIGH, LOW, LOW},
-  {HIGH, LOW, LOW, LOW, HIGH},
-  {LOW, HIGH, HIGH, HIGH, LOW}
+//0 is off, 1 is on
+//a sketch for a smiley face
+int smileyFace[5][5] = {
+  {0, 1, 0, 1, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 1, 0, 0},
+  {1, 0, 0, 0, 1},
+  {0, 1, 1, 1, 0}
 };
+
+int sadFace[5][5] = {
+  {1, 0, 0, 0, 1},
+  {0, 0, 0, 0, 0},
+  {0, 0, 1, 0, 0},
+  {0, 1, 1, 1, 0},
+  {1, 0, 0, 0, 1}
+};
+
+int heart[5][5] = {
+  {0, 1, 0, 1, 0},
+  {1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 1},
+  {0, 1, 1, 1, 0},
+  {0, 0, 1, 0, 0}
+};
+
+int alligator[5][5] = {
+  {0, 1, 1, 1, 0},
+  {1, 1, 0, 1, 1},
+  {0, 0, 1, 1, 1},
+  {1, 1, 1, 1, 1},
+  {0, 1, 1, 1, 0}
+};
+
 
 long previousMillis = 0;
 long interval = 1000;
@@ -22,17 +49,22 @@ void setup() {
 }
 
 void loop() {
-  drawFigure(smiley);
+  drawFigure(smileyFace);
 }
 
 void drawFigure(int figure[5][5]) {
+  //initializes the current milliseconds since the program started
   unsigned long currentMillis = millis();
   
-  while(currentMillis - previousMillis > interval) {
+  //repeatedly lights up the LEDs per the predetermined pattern
+  //until the milliseconds reaches the interval
+  while(currentMillis - previousMillis < interval) {
     for(int i = 0; i < 5; i++){
-      for(int j = 0; j < 5; j++) {
-        digitalWrite(rows[i], figure[i][j]);
-        if(figure[i][j] == HIGH) {
+      for(int j = 0; j < 5; j++) {   
+        //if the row is on, set the column to low
+        //and turn switch the LED off after a 1 millisecond delay
+        if(figure[i][j] == 1) {
+          digitalWrite(rows[i], HIGH);
           digitalWrite(cols[j], LOW);
           delay(1);
           digitalWrite(cols[j], HIGH);
@@ -40,7 +72,40 @@ void drawFigure(int figure[5][5]) {
         }
       }
     }
+    //reset current milliseconds
+    currentMillis = millis();
   }
   
+  //reset the previous milliseconds to in order to
+  //draw another figure.
+  previousMillis = currentMillis;
+}
+
+void drawInverseFigure(int figure[5][5]) {
+  //initializes the current milliseconds since the program started
+  unsigned long currentMillis = millis();
+  
+  //repeatedly lights up the LEDs per the predetermined pattern
+  //until the milliseconds reaches the interval
+  while(currentMillis - previousMillis < interval) {
+    for(int i = 0; i < 5; i++){
+      for(int j = 0; j < 5; j++) {
+        //if the row is on, set the column to low
+        //and turn switch the LED off after a 1 millisecond delay
+        if(figure[i][j] == 0) {
+          digitalWrite(rows[i], HIGH);
+          digitalWrite(cols[j], LOW);
+          delay(1);
+          digitalWrite(cols[j], HIGH);
+          digitalWrite(rows[i], LOW);
+        }
+      }
+    }
+    //reset current milliseconds
+    currentMillis = millis();
+  }
+  
+  //reset the previous milliseconds to in order to
+  //draw another figure.
   previousMillis = currentMillis;
 }
