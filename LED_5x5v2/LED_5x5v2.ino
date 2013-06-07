3d1283dc3a5ad4557756af2ef5e81b0f5f6e0e37
Empty Arsenal <matt.merkes@gmail.com>
2013-06-07 08:41:50 -0700

    revised and simplified code for drawFigure method

--- LED_5x5v2/LED_5x5v2.ino
+++ LED_5x5v2/LED_5x5v2.ino
@@ -12,9 +12,9 @@ int smileyFace[5][5] = {
 };
 
 int sadFace[5][5] = {
-  {1, 0, 0, 0, 1},
+  {1, 1, 0, 1, 1},
+  {1, 1, 0, 1, 1},
   {0, 0, 0, 0, 0},
-  {0, 0, 1, 0, 0},
   {0, 1, 1, 1, 0},
   {1, 0, 0, 0, 1}
 };
@@ -36,8 +36,8 @@ int alligator[5][5] = {
 };
 
 
-long previousMillis = 0;
-long interval = 1000;
+unsigned long previousMillis = 0;
+long interval = 5000;
 
 void setup() {
   
@@ -50,6 +50,9 @@ void setup() {
 
 void loop() {
   drawFigure(smileyFace);
+  drawFigure(sadFace);
+  drawFigure(heart);
+  drawFigure(alligator);
 }
 
 void drawFigure(int figure[5][5]) {
@@ -64,35 +67,6 @@ void drawFigure(int figure[5][5]) {
         //if the row is on, set the column to low
         //and turn switch the LED off after a 1 millisecond delay
         if(figure[i][j] == 1) {
-          digitalWrite(rows[i], HIGH);
-          digitalWrite(cols[j], LOW);
-          delay(1);
-          digitalWrite(cols[j], HIGH);
-          digitalWrite(rows[i], LOW);
-        }
-      }
-    }
-    //reset current milliseconds
-    currentMillis = millis();
-  }
-  
-  //reset the previous milliseconds to in order to
-  //draw another figure.
-  previousMillis = currentMillis;
-}
-
-void drawInverseFigure(int figure[5][5]) {
-  //initializes the current milliseconds since the program started
-  unsigned long currentMillis = millis();
-  
-  //repeatedly lights up the LEDs per the predetermined pattern
-  //until the milliseconds reaches the interval
-  while(currentMillis - previousMillis < interval) {
-    for(int i = 0; i < 5; i++){
-      for(int j = 0; j < 5; j++) {
-        //if the row is on, set the column to low
-        //and turn switch the LED off after a 1 millisecond delay
-        if(figure[i][j] == 0) {
           digitalWrite(rows[i], HIGH);
           digitalWrite(cols[j], LOW);
           delay(1);
